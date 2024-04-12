25ce2cdd69ab9e6a436aec217d5f9792ad123819
Ian McLerran <imclerran@protonmail.com>
2024-04-12 14:23:49 -0500

    update urls to be api endpoints only

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -3,7 +3,6 @@ from .views import index
 from .views import BookingView, MenuView
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('api/bookings/', BookingView.as_view(), name='bookings'),
-    path('api/menu/', MenuView.as_view(), name='menu'),
+    path('bookings/', BookingView.as_view(), name='bookings'),
+    path('menu/', MenuView.as_view(), name='menu'),
 ]
